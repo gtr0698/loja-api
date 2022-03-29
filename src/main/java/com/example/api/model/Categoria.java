d9451e8ec162f3ccde6bd4d4c9e84d30dad0cca0
Guilherme <guilherme_taborda@live.com>
2022-03-29 17:14:56 -0300

    Criando Model Categoria

---
+++ src/main/java/com/example/api/model/Categoria.java
@@ -0,0 +1,52 @@
+package com.example.api.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @NotBlank
+    private String nome;
+
+    @NotNull
+    @NotBlank
+    private String descricao;
+
+    public Categoria(String nome, String descricao) {
+        this.nome = nome;
+        this.descricao = descricao;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public Categoria atualizaCategoria(String nome, String descricao){
+        this.nome = nome;
+        this.descricao = descricao;
+
+        return this;
+    }
+
+    public Categoria() {
+        super();
+    }
+}
