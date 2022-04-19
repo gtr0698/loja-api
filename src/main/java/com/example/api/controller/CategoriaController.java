b6903c0dc139a612543618cf4edb5b7feaca78af
Guilherme <guilherme_taborda@live.com>
2022-04-19 19:13:02 -0300

    Removendo o init binder

--- src/main/java/com/example/api/controller/CategoriaController.java
+++ src/main/java/com/example/api/controller/CategoriaController.java
@@ -3,12 +3,10 @@ package com.example.api.controller;
 import com.example.api.dto.categoria.CategoriaResponseDto;
 import com.example.api.dto.categoria.CreateCategoriaRequestDto;
 import com.example.api.dto.categoria.UpdateCategoriaRequestDto;
-import com.example.api.exception.ProibeNomeDuplicadoCategoriaValidator;
 import com.example.api.service.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.Validator;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,14 +20,6 @@ public class CategoriaController {
 
     @Autowired
     private CategoriaService categoriaService;
-
-    @Autowired
-    private ProibeNomeDuplicadoCategoriaValidator proibeNomeDuplicadoCategoriaValidator;
-
-    @InitBinder
-    public void init(WebDataBinder binder){
-        binder.addValidators(proibeNomeDuplicadoCategoriaValidator);
-    }
 
     @GetMapping
     public ResponseEntity<List<CategoriaResponseDto>>listar(Pageable pageable){
