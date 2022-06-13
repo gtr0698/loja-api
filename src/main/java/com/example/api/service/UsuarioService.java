0fbe23d4185b378e54e23383d6d49444966d786b
Guilherme <guilherme_taborda@live.com>
2022-06-13 16:39:34 -0300

    Atualizando UsuarioService

--- src/main/java/com/example/api/service/UsuarioService.java
+++ src/main/java/com/example/api/service/UsuarioService.java
@@ -8,9 +8,11 @@ import com.example.api.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+@Service
 public class UsuarioService {
 
     @Autowired
